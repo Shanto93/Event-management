128e9130425830d7e26e4c9d83d7d45b00de97cc
Shanta Islam <shantoislam7363@gmail.com>
2023-10-07 00:03:43 +0600

    Initial setup has been done and also different folder for routes is implemented

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDQnDXLloSdNFKhyIeet3UL_ZsuaH4CITw",
+  authDomain: "event-management-9111f.firebaseapp.com",
+  projectId: "event-management-9111f",
+  storageBucket: "event-management-9111f.appspot.com",
+  messagingSenderId: "728848731037",
+  appId: "1:728848731037:web:3fa093e908b9e688eafd74"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div>
+            <div className="text-2xl">This is Home</div>
+        </div>
+    );
+};
+
+export default Home;
