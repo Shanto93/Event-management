e3053c6453d79ed7419ae35ec4a28fd5f5247eb0
Shanta Islam <shantoislam7363@gmail.com>
2023-10-09 20:27:44 +0600

    404 error page modified

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,8 +1,12 @@
 const ErrorPage = () => {
     return (
-        <div>
-            <h2>OOpssss!!!!!</h2>
+        <div className="min-h-screen flex items-center justify-center">
+        <div className="text-center">
+            <h1 className="text-6xl font-bold text-red-500">404</h1>
+            <p className="text-2xl text-gray-600">Page Not Found</p>
+            <a href="/" className="mt-4 inline-block px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Go Back Home</a>
         </div>
+    </div>
     );
 };
 
